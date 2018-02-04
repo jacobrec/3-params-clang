b252a3722355a8d8a744f804ef870862677e6b04
Jacob Reckhard <jacobreckhard@gmail.com>
2018-02-04 11:51:34 -0700

    setup project

---
+++ Param3Max.cpp
@@ -0,0 +1,24 @@
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Lex/Lexer.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Execution.h"
+#include "clang/Tooling/Refactoring.h"
+#include "clang/Tooling/Refactoring/AtomicChange.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/Signals.h"
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+using namespace llvm;
+
+
+int main(int argc, const char **argv) {
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Build instructions
+1. Clone project to llvm/tools/clang/tools/extra
+2. Navigate to llvm/tools/clang/tools/extra
+3. Add the line "add_subdirectory(3-params-max)" to CMakeLists.txt
+4. Navigate into the folder llvm/build
+5. Build the 3-params-max tool using your build tool of choice, eg) `ninja 3-params`
